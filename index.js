a371389e3df17824029bf694a7ee9f16c8c0af44
JUdihouma04 <sheldonodhiuma04@gmail.com>
2022-08-18 18:14:54 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(spy) {
+  spy();
+}
+function returnsANamedFunction() {
+  return function parameter() {
+    return "parameter";
+  };
+}
+function returnsAnAnonymousFunction() {
+  return function () {};
+}
